eb52dd48d884d6b99cbd66cffc7c7d53bc6bd576
Oleksii <oleksii@macbook-air-m1---oleksii.home>
2023-07-04 20:11:12 +0200

    Setup

--- App.js
+++ App.js
@@ -4,6 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Hello</Text>
+      <View>
+        <Text>
+          Hi
+        </Text>
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
